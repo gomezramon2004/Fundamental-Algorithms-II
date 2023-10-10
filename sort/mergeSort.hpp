1db8a78695fc3fd621e174633f718f342715c360
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-10-10 01:44:53 -0700

    eliminated left right arguments

--- sort/mergeSort.hpp
+++ sort/mergeSort.hpp
@@ -7,4 +7,4 @@ bool compareInfo(const Info& info1, const Info& info2);
 // Merge sort algorithm: merge section
 void merge(LinkedList& vec, int left, int mid, int right);
 // Merge sort algorithm: recursive section
-void mergeSort(LinkedList& vec, int left, int right);
+void mergeSort(LinkedList& vec);
