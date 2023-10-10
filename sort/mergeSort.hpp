1caee50119a2d5752022d633c19ed165bd4e4c11
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-10-10 01:47:16 -0700

    mergesort header completed

--- sort/mergeSort.hpp
+++ sort/mergeSort.hpp
@@ -4,7 +4,7 @@
 
 // Compare the vectors based on their UBIs and Dates
 bool compareInfo(const Info& info1, const Info& info2);
-// Merge sort algorithm: merge section
-void merge(LinkedList& vec, int left, int mid, int right);
+// Merge sort algorithm: merge-lists section
+void mergeLists(LinkedList& left, LinkedList& right);
 // Merge sort algorithm: recursive section
-void mergeSort(LinkedList& vec);
+void mergeSort(LinkedList& list);
