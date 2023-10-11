6dcd8ac6282a0b3eb83522b8517fc79069a31bda
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-10-10 22:29:44 -0700

    fixed bug related to print and segmentation fault 11

--- print/print.cpp
+++ print/print.cpp
@@ -4,24 +4,6 @@
 #include <string>
 #include <map>
 
-/*
-    To replicate the same format as the input, std::setfill and std::setw will reserve the 
-    field width (in this case, two zeros) and place the date value to the right. tm_mon has 
-    a range of 0 to 11, and tm_year starts counting from the year 1900.
-*/
-
-// void print(const LinkedList& list) {
-//     for (auto &&i : list) {
-//         std::cout << std::setfill('0') << std::setw(2) << i.dateTime.tm_mday << '/' << 
-//         std::setfill('0') << std::setw(2) << i.dateTime.tm_mon + 1 << '/' << 
-//         i.dateTime.tm_year + 1900 << ' ' << 
-//         std::setfill('0') << std::setw(2) << i.dateTime.tm_hour << ":" << 
-//         std::setfill('0') << std::setw(2) << i.dateTime.tm_min << " " << 
-//         i.enterPoint << " " << 
-//         i.ubi << "\n";
-//     }
-// }
-
 std::string get_month_name(int index) {
    std::map<int, std::string> monthNames {
         {1, "jan"},
@@ -47,10 +29,13 @@ std::string get_month_name(int index) {
 void print(const LinkedList& list1, const LinkedList& list2) {
     Node* temp1 = list1.head;
     Node* temp2 = list2.head;
+    size_t i = 0, year = 0;
 
-    for (size_t i = 0; i < 12; i++) {
-        int counter1 = 0;
-        int counter2 = 0;
+    temp1->info.dateTime.tm_mon < temp2->info.dateTime.tm_mon ? i = temp1->info.dateTime.tm_mon : i = temp1->info.dateTime.tm_mon;
+    temp1->info.dateTime.tm_year < temp2->info.dateTime.tm_year ? year = temp1->info.dateTime.tm_year : year = temp1->info.dateTime.tm_year;
+
+    while (temp1 || temp2) {
+        int counter1 = 0, counter2 = 0;
 
         while (temp1 && temp1->info.dateTime.tm_mon == i) {
             ++counter1;
@@ -58,11 +43,12 @@ void print(const LinkedList& list1, const LinkedList& list2) {
         }
 
         while (temp2 && temp2->info.dateTime.tm_mon == i) {
-            ++counter2;
+            ++counter2; 
             temp2 = temp2->next;
         }
 
-        std::cout << get_month_name(i + 1) << " " << temp1->info.dateTime.tm_year - 100 << " " << counter1 << " " << counter2 << "\n";
+
+        std::cout << get_month_name(i + 1) << " " << year - 100  << " " << counter1 << " " << counter2 << "\n";
+        i + 1 == 12 ? ++year, i = 0 : ++i;
     }
 }
-
